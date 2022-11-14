3eee796f8f082856e8f0afa8219384b34b3e4a34
wouter <wouter.nijssen@bokkz.nl>
2022-11-14 14:27:34 +0100

    Toevoegen interface voor de calculator en de interface implementatie voor de berekening van de verschuldigde belasting.

--- src/main/java/nl/uva/api/Endpoint.java
+++ src/main/java/nl/uva/api/Endpoint.java
@@ -2,15 +2,8 @@ package nl.uva.api;
 
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.*;
-
 @Component
-@Path("/api/v1/")
 public class Endpoint {
 
-    @GET
-    @Path("/awesome/")
-    public Object getAwesome() {
-        return "Awesome";
-    }
+
 }
