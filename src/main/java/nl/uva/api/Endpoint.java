12d7d2653187b1f90a935f0a896a37bc12434f54
wouter <wouter.nijssen@bokkz.nl>
2022-11-17 09:59:05 +0100

    Endpoint.java

--- src/main/java/nl/uva/api/Endpoint.java
+++ src/main/java/nl/uva/api/Endpoint.java
@@ -2,8 +2,36 @@ package nl.uva.api;
 
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 @Component
+@Path("/api/v1")
 public class Endpoint {
 
+    @Path("/awesome")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAwesome() {
+        return "Awesome";
+    }
+
+    @Path("/awesome2/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAwesome(@PathParam("id") String id) {
+        if ("1".equals(id)) {
+            return "Awesome";
+        }
+        else if ("2".equals(id)) {
+            return "Also Awesome";
+        }
+        else {
+            return "Not Awesome";
+        }
+    }
 
 }
